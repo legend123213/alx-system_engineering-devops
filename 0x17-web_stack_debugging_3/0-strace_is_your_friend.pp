0305632ce5452e64a0cf4af8bfae0d7eea1e8c89
Abel Wendmu <abelwendmu124@gmail.com>
2024-05-12 23:25:38 +0300

    deb

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,5 @@
+# Fixes a wordpress site returning a error, running on apache2
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
+}
